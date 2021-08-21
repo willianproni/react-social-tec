d89542bb5517f26a1d6c4b75cd99ab08ca702eea
willianproni <wifepro@hotmail.com>
2021-08-21 12:14:43 -0300

    teste

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+  background-color: #fff;
+  border-radius: 8px;
+
+  #_next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+}
+`
+
+
+
+export default function Home() {
+  return (
+  
+  <main>
+    
+    <Box>
+      Imagem
+    </Box>
+
+    <Box>
+    Bem vindo
+    </Box>
+
+    <Box>
+      Comunidades
+    </Box>
+
+  </main>
+
+  
+)}
