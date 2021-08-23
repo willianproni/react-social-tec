7c55342d005cd32df613049674f342c80d437bf4
willianproni <wifepro@hotmail.com>
2021-08-22 22:14:10 -0300

    aula2 incompleta

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
+import { useState } from "react"
 
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations"
 
 //------------------------------------------------------------
@@ -12,12 +13,28 @@ import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations"
     return(
       <Box>
         <img style={{ borderRadius: '5px' }} src={`https://github.com/${Usuario.gitHubUser}.png`}/>
+        <hr />
+      <p>
+        <a className="boxLink" href={`http://github.com/${Usuario.gitHubUser}`}>
+        @{Usuario.gitHubUser}
+        </a>
+      </p>
+
+        <hr />
+
+        <AlurakutProfileSidebarMenuDefault/>
      </Box>
     )
   }
 
 export default function Home() {
+  React.useState(['Will o maior']);
+
   const GitHubUser = 'willianproni';
+
+  const [comunidades, setComunidades] = React.useState(['Will o maior']);
+  // const comunidades = comunidades[0];
+  //const alteradorDeComunidades = comunidades[1];
 
   const FriendsUser = [
     'juunegreiros',
@@ -31,7 +48,7 @@ export default function Home() {
   return (
     <>
 
-    <AlurakutMenu/>
+  <AlurakutMenu/>
 
   <MainGrid>
 
@@ -45,12 +62,70 @@ export default function Home() {
           Bem vindo(a) {GitHubUser}
         </h1>
 
-      <OrkutNostalgicIconSet/>
+      
+      </Box>
+
+      <Box>
+        <h2 className="subTitle">O que deseja fazer?</h2>
+
+        <form onSubmit={function handleCreateComunity(e){
+          e.preventDefault();
+          console.log(e);
+
+          setComunidades(['willianproni'])
+          console.log(comunidades)
+
+          'Alura 5tars'
+        }}>
+
+          <div>
+            <input 
+              placeholder="Digite seu pensamento" 
+              name="Title" 
+              aria-label="Digite seu pensamento"
+              type="Text"
+              />  
+
+          </div>
+
+          <div>
+            <input 
+              placeholder="Digite seu pensamento" 
+              name="Title" 
+              aria-label="Digite seu pensamento"
+              type="text"
+              />  
+          </div>
+
+        <button>
+          Criar comunidade
+        </button>
+        <button>
+          Publicar
+        </button>
+
+
+        
+         
+        </form>
 
       </Box>
     </div>
 
     <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }} >
+      <ProfileRelationsBoxWrapper>
+      <ul>
+        {comunidades.map((Friends) => {
+          return(
+            <li>  
+              <a href={`/users/${Friends}`}>
+              <img src={`https://github.com/${Friends}.png`} />
+              <span>{Friends}</span>
+          </a></li>
+          )
+        })}
+        </ul>
+      </ProfileRelationsBoxWrapper>
       <ProfileRelationsBoxWrapper>
         
         <h2 className='smallTitle'>
